ee2da956b1a285c91d448f875021a771ddc6f5c7
spokuta <druzhko.r@gmail.com>
2025-11-23 21:49:37 +0200

    1

--- helloworlld/src/main/kotlin/Main.kt
+++ helloworlld/src/main/kotlin/Main.kt
@@ -1,24 +1,20 @@
 fun seed(): String = "spokuta"
 
-fun labNumber(): Int = 1
+fun labNumber() : Int = 1
 
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-
-    // Кошеня №1
     var kitty = "Васько"
     kitty += " \uD83D\uDC31"
     val age = 7
     println("Кошеня №1 - $kitty віком $age років")
 
-    // Кошеня №2
     val catName: String = "Мурзик \uD83D\uDC08"
     val weight: Float = 3.5f
     println("Кошеня №2 - $catName з вагою $weight кг")
 
-    // Кошеня №3 (Нове завдання)
-    val name3 = "Рудий \uD83D\uDC06"
-    val age3 = 6
-    val weight3 = 8.2
+    val name3: String = "Рудий \uD83D\uDC06"
+    val age3: Int = 6
+    val weight3: Float = 8.2f
     println("Кошеня №3 - $name3 віком $age3 років та вагою $weight3 кг")
 }
